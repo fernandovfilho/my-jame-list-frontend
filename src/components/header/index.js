70f0a07f68eef2e7a120d6d51240ddceb39d874f
Fernando Vargas Filho <fernandov.filho@gmail.com>
2021-05-28 02:04:55 -0300

    feat: colors file, components size test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,26 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import logo from "../../img/logo.png";
 import "./style.scss";
 
 export function Header() {
   return (
     <div className="container">
-      <div className="logo" />
-      <form action="#">
-        <div className="search-container">
-          <input
-            className="search-input"
-            type="text"
-            id="search"
-            name="search"
-          />
-          <FontAwesomeIcon className="search-input-icon" icon={faSearch} />
-        </div>
-      </form>
+      <div className="header">
+        <img src={logo} alt="MyJameList" className="logo" />
+        <form action="#">
+          <div className="search-container">
+            <input
+              className="search-input"
+              type="text"
+              id="search"
+              name="search"
+            />
+            <FontAwesomeIcon className="search-input-icon" icon={faSearch} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Header } from "../../components/header";
 import "./style.scss";
 
 export function HomePage() {
   return (
     <>
-      <Header />
       <div className="home-page"></div>
     </>
   );
